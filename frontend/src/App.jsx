25b2f0aecc2d0475098bae36646345493e164cd9
Josué García <134705823+JosueGarciaAbata@users.noreply.github.com>
2025-04-24 14:32:40 -0500

    Revert "Feature/progress panel UI"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,8 @@
-import { Route, Routes } from "react-router-dom"
-import "./App.css"
-import AuthLayout from "./layouts/AuthLayout"
-import Login from "./pages/Login"
-import Register from "./pages/Register"
-import Panel from "./features/progress-panel/components/Panel"
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import AuthLayout from "./layouts/AuthLayout";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
@@ -17,15 +16,14 @@ function App() {
           <Route path="register" element={<Register />} />
         </Route>
 
-        <Route path="/goals-progress" element={<Panel />} />
-        <Route path="goals">
+        <Route path="concerts">
           {/* <Route index element={<ConcertsHome />} /> */}
           {/* <Route path=":city" element={<City />} /> */}
           {/* <Route path="trending" element={<Trending />} /> */}
         </Route>
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
