f4bb0bc5dde56b5c7d2eb99422f27b90f9fc8d6b
DavidJosueP2 <deividjosue52@gmail.com>
2025-04-24 10:39:11 -0500

    Redefine Category Model

--- backend/app/Models/Category.php
+++ backend/app/Models/Category.php
@@ -4,7 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Goal;
 
+/**
+ * Class Category
+ *
+ * Represents a financial goal category (e.g., Travel, Savings).
+ *
+ * @property int $id
+ * @property string $name
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ */
 class Category extends Model
 {
     use HasFactory;
@@ -16,4 +27,3 @@ class Category extends Model
         return $this->hasMany(Goal::class);
     }
 }
-
