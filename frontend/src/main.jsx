01ef2f871a3936dddd619046276df8254deec36e
Josué García <josuegarcab2@hotmail.com>
2025-04-24 09:57:16 -0500

    Add dependencies

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,14 +2,26 @@ import { createRoot } from "react-dom/client"
 import "./index.css"
 import App from "./App.jsx"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material"
+
+const theme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#1976d2",
+    },
+  },
+})
 
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById("root")).render(
-  <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <App />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </BrowserRouter>
-  </QueryClientProvider>
+  <ThemeProvider theme={theme}>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </BrowserRouter>
+    </QueryClientProvider>
+  </ThemeProvider>
 )
