d7993d4c87fb5da1400b0433fb3d00ecfc3ff84a
DavidJosueP2 <deividjosue52@gmail.com>
2025-04-24 14:12:47 -0500

    Redifine Controllers

---
+++ backend/app/Http/Controllers/CategoryController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Category;
+use Illuminate\Http\JsonResponse;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of all available categories.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index(): JsonResponse
+    {
+        $categories = Category::orderBy('name')->get();
+
+        return response()->json([
+            'status' => 'success',
+            'categories' => $categories
+        ]);
+    }
+}
+
