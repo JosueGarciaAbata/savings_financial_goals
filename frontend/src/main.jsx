c0af8cfe3a31be1181a5225fd24f32605af214cb
Josué García <josuegarcab2@hotmail.com>
2025-04-25 08:35:16 -0500

    Fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,6 @@ import App from "./App.jsx"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { CssBaseline, ThemeProvider, createTheme } from "@mui/material"
 import { BrowserRouter } from "react-router-dom"
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 
 const theme = createTheme({
   palette: {
@@ -22,7 +21,6 @@ createRoot(document.getElementById("root")).render(
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <App />
-        <ReactQueryDevtools initialIsOpen={false} />
       </BrowserRouter>
     </QueryClientProvider>
   </ThemeProvider>
